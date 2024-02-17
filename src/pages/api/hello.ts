b006c3304f010bf1a72a04ae2d1340dc2e419bc3
toono-f <52028799+toono-f@users.noreply.github.com>
2024-02-17 13:05:33 +0900

    Refactor API handler function

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,15 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
-};
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: "John Doe" });
+export default function handler() {
+  return Response.json({ name: "John Doe" });
 }
 
 export const config = {
