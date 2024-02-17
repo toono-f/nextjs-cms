523eede3d452bad9f4c6fc1d81a9f8ea77e6036d
toono-f <52028799+toono-f@users.noreply.github.com>
2024-02-17 18:14:41 +0900

    Remove unused import statement

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import PostType from "@/types/PostType";
 import PostService from "@/serivces/PostService";
-import { useEffect } from "react";
 
 const Home: NextPage<{
   staticPostList: PostType[]; // 型の指定をする場所に注意！
